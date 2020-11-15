1f50e15d3805aabf12d2ce816566b1a110d83908
NikolaevArtem <artem_nikolaev69@mail.ru>
2020-11-15 21:06:33 +0300

    without setMessage

--- public/homePage.js
+++ public/homePage.js
@@ -1,1 +1,97 @@
 'use strict'
+
+//Выход из личного кабинета
+const userLogOut = new LogoutButton();
+
+userLogOut.action = () => ApiConnector.logout((response) => {
+  if (response.success) {
+      location.reload();
+  }
+});
+  
+//Информация о пользователе
+ApiConnector.current((response) => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+    }
+})
+
+//Получение текущих курсов валюты
+const newRates = new RatesBoard();
+newRates.getRates = () => ApiConnector.getStocks((response) => {
+    if (response.success) {
+        newRates.clearTable();
+        newRates.fillTable(response.data);
+    }
+})
+setInterval(() => newRates.getRates(), 6000);
+
+
+//Операции с деньгами
+const newBalance = new MoneyManager();
+
+//Пополнение баланса
+newBalance.addMoneyCallback = (data) => ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);   
+    }
+    //Вывод сообщения?? Не получается вывести сообщение
+    // newBalance.setMessage(risSuccess, message)   
+});
+
+//Конвертация 
+newBalance.conversionMoneyCallback = (data) => ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);  
+    }
+    //Вывод сообщения?? Не получается вывести сообщение
+    // newBalance.setMessage(risSuccess, message)  
+})
+
+//Перевод
+newBalance.sendMoneyCallback = (data) => ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);   
+    }
+     //Вывод сообщения?? Не получается вывести сообщение
+    // newBalance.setMessage(risSuccess, message) 
+})
+
+//Избранные
+const friend = new FavoritesWidget();
+
+//Вывод списка
+ApiConnector.getFavorites((response) => {
+    if (response.success) {
+        friend.clearTable(response.data);
+        friend.fillTable(response.data);
+        newBalance.updateUsersList(response.data);
+    }
+})
+
+//Добавление в список
+friend.addUserCallback = (data) => ApiConnector.addUserToFavorites(data, (response) => {
+    if (response.success) {
+        friend.clearTable(response.data);
+        friend.fillTable(response.data);
+        newBalance.updateUsersList(response.data);
+    }
+    //Вывод сообщения?? Не получается вывести сообщение
+    // newBalance.setMessage(risSuccess, message)
+})
+
+
+//Удаление из списка
+friend.removeUserCallback = (data) => ApiConnector.removeUserFromFavorites(data, (response) => {
+    if (response.success) {
+        friend.clearTable(response.data);
+        friend.fillTable(response.data);
+        newBalance.updateUsersList(response.data);
+    }
+    //Вывод сообщения?? Не получается вывести сообщение
+    // newBalance.setMessage(risSuccess, message)
+})
+
+
+ 
+  
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,22 +1,26 @@
 
 'use strict'
 
-const newUser = new UserForm()
+const newUser = new UserForm();
 
+//Авторизация пользователя
 newUser.loginFormCallback = (data) => ApiConnector.login(data, (response) => {
-  if (response.success === true) {
-    location.reload()
+  if (response.success) {
+    location.reload();
   } else {
-    newUser.setLoginErrorMessage(response.error)
+    newUser.setLoginErrorMessage(response.error);
   }
 })
+
+//Регистрация пользователя
 newUser.registerFormCallback = (data) => ApiConnector.register(data, (response) => {
-  if (response.success === true) {
-    location.reload()
+  if (response.success) {
+    location.reload();
   } else {
-    newUser.setRegisterErrorMessage(response.error)
+    newUser.setRegisterErrorMessage(response.error);
   }
 })
+
 
 
 
