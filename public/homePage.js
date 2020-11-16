bda92e02f99e95162e1c8e33ae4402b507a3484d
NikolaevArtem <artem_nikolaev69@mail.ru>
2020-11-16 21:58:35 +0300

    fixed

--- public/homePage.js
+++ public/homePage.js
@@ -18,13 +18,15 @@ ApiConnector.current((response) => {
 
 //Получение текущих курсов валюты
 const newRates = new RatesBoard();
-newRates.getRates = () => ApiConnector.getStocks((response) => {
+newRates.getRates = () =>  ApiConnector.getStocks((response) => {
     if (response.success) {
         newRates.clearTable();
         newRates.fillTable(response.data);
     }
 })
+newRates.getRates()
 setInterval(() => newRates.getRates(), 6000);
+
 
 
 //Операции с деньгами
@@ -33,32 +35,32 @@ const newBalance = new MoneyManager();
 //Пополнение баланса
 newBalance.addMoneyCallback = (data) => ApiConnector.addMoney(data, (response) => {
     if (response.success) {
-        ProfileWidget.showProfile(response.data);   
-    }
-    //Вывод сообщения?? Не получается вывести сообщение.
-    //Как понял: isSuccess => response.success(true || false),
-    //message => response.error || тут затрудняюсь
-    //Вывода в блок ни какого у меня не происходит
-    //Прошу совета!
-    // newBalance.setMessage(isSuccess, message)   
+        ProfileWidget.showProfile(response.data);
+        newBalance.setMessage(response.success, 'Ваш баланс пополнен');// Не заметил сразу окно вывода. Спасибо! 
+    } else {
+        newBalance.setMessage(response.success, response.error);
+      }  
 });
 
 //Конвертация 
 newBalance.conversionMoneyCallback = (data) => ApiConnector.convertMoney(data, (response) => {
     if (response.success) {
-        ProfileWidget.showProfile(response.data);  
-    }
-    //Вывод сообщения?? Не получается вывести сообщение
-    // newBalance.setMessage(risSuccess, message)  
+        ProfileWidget.showProfile(response.data); 
+        newBalance.setMessage(response.success, 'Конвертирование прошло успешно');
+    } else {
+        newBalance.setMessage(response.success, response.error);
+      }
 })
 
 //Перевод
 newBalance.sendMoneyCallback = (data) => ApiConnector.transferMoney(data, (response) => {
+    console.log(data)
     if (response.success) {
-        ProfileWidget.showProfile(response.data);   
-    }
-     //Вывод сообщения?? Не получается вывести сообщение
-    // newBalance.setMessage(risSuccess, message) 
+        ProfileWidget.showProfile(response.data); 
+        newBalance.setMessage(response.success, 'Перевод выполнен');
+    } else {
+        newBalance.setMessage(response.success, response.error);
+      } 
 })
 
 //Избранные
@@ -79,9 +81,10 @@ friend.addUserCallback = (data) => ApiConnector.addUserToFavorites(data, (respon
         friend.clearTable(response.data);
         friend.fillTable(response.data);
         newBalance.updateUsersList(response.data);
-    }
-    //Вывод сообщения?? Не получается вывести сообщение
-    // newBalance.setMessage(risSuccess, message)
+        newBalance.setMessage(response.success, `Пользователь ${data.name} добавлен в друзья`);
+    } else {
+        newBalance.setMessage(response.success, response.error);
+      }
 })
 
 
@@ -91,9 +94,10 @@ friend.removeUserCallback = (data) => ApiConnector.removeUserFromFavorites(data,
         friend.clearTable(response.data);
         friend.fillTable(response.data);
         newBalance.updateUsersList(response.data);
-    }
-    //Вывод сообщения?? Не получается вывести сообщение
-    // newBalance.setMessage(risSuccess, message)
+        newBalance.setMessage(response.success, 'Пользователь удалён из друзей');
+    } else {
+        newBalance.setMessage(response.success, response.error);
+      }
 })
 
 
